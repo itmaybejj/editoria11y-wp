ece245283bb46d92cf678b2b6bed275107331deb
John Jameson <jjameson@princeton.edu>
2023-12-11 12:30:15 -0500

    code style.

--- src/functions.php
+++ src/functions.php
@@ -159,7 +159,7 @@ add_action( 'enqueue_block_assets', 'ed11y_enqueue_editor_content_assets' );
 /**
  * Returns page-specific config for the Editoria11y library.
  *
- * @param Object $user WP_User
+ * @param Object $user WP_User.
  */
 function ed11y_get_params( $user ) {
 
