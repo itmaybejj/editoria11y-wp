a1856e91c1cdb45dd2de0adeebbbadcf9938cd1c
John Jameson <jjameson@princeton.edu>
2024-06-26 16:10:09 -0400

    Don't render dashboard when tables fail.

--- editoria11y.php
+++ editoria11y.php
@@ -262,7 +262,7 @@ class Editoria11y {
 		if ( $tableCheck === '1.1-failed' ) {
 			// Tables are broken, don't try again until next release
 			return false;
-		} else if ( $tableCheck !== '1.1' ) {
+		} else if ( $tableCheck !== '1.2' ) {
 			// Create DB and set option based on success
 			update_option( "editoria11y_db_version", '1.1-failed' );
 			self::create_database();
