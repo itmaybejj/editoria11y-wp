761cab7e41125d5aa704b37efbde4b4734512bb0
John Jameson <jjameson@princeton.edu>
2025-01-03 16:27:49 -0500

    Add edit link, update library.

--- assets/js/editoria11y-wp.js
+++ assets/js/editoria11y-wp.js
@@ -185,33 +185,73 @@ const ed11yCustomTests = function() {
     });
 };
 
+const ed11yInit = function() {
+  let ed11yOpts = document.getElementById('editoria11y-init');
+  if (!!ed11yOpts && window.location.href.indexOf('elementor-preview') === -1) {
+    ed11yOptions = JSON.parse(ed11yOpts.innerHTML);
+    ed11yOptions.customTests = 1;
+    ed11yOptions.panelNoCover = '#edac-highlight-panel'; // Accessibility Checker module
+
+    ed11yOptions.linkStringsNewWindows = ed11yOptions.linkStringsNewWindows ? new RegExp(ed11yOptions.linkStringsNewWindows, 'g') : /window|\stab|download/g;
+    if (ed11yOptions.title.length < 3) {
+      ed11yOptions.title = document.title;
+    }
+    ed11yOptions['checkVisible'] = ed11yOptions['checkVisible'] === 'true';
+    // When triggered by the in-editor "issues" link, force assertive.
+    if (window.location.href.indexOf('preview=true') > -1) {
+      ed11yOptions['alertMode'] = 'assertive';
+    }
+    if (window.location.href.indexOf('ed1ref') > -1) {
+      ed11yOptions['alertMode'] = 'assertive';
+      ed11yOptions['showDismissed'] = true;
+    }
+    ed11yOptions.cssUrls = [ed11yOptions.cssLocation];
+
+
+    const editLink = document.querySelector('#wp-admin-bar-edit a');
+    // todo: this is not detected due to race condition.
+    const elementorLink = document.querySelector('#wp-admin-bar-elementor_edit_page a');
+    if (editLink || elementorLink) {
+      const editIcon = document.createElement('span');
+      editIcon.classList.add('ed11y-custom-edit-icon');
+      editIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152L0 424c0 48.6 39.4 88 88 88l272 0c48.6 0 88-39.4 88-88l0-112c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 112c0 22.1-17.9 40-40 40L88 464c-22.1 0-40-17.9-40-40l0-272c0-22.1 17.9-40 40-40l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24L88 64z"/></svg>';
+      const reLink = function(link) {
+        const linkButton = document.createElement('a');
+        linkButton.href = link.href;
+        linkButton.textContent = link.textContent;
+        linkButton.prepend(editIcon.cloneNode(true));
+        return linkButton;
+      }
+      const editLinks = document.createElement('div');
+      if (editLink) {
+        editLinks.appendChild(reLink(editLink));
+      }
+      if (elementorLink) {
+        editLinks.appendChild(reLink(elementorLink));
+      }
+      ed11yOptions.editLinks = editLinks;
+
+      // todo: Add param for listener to hide edit links on certain widgets.
+      /*if (!!drupalSettings.editoria11y.hide_edit_links) {
+        document.addEventListener('ed11yPop', e => {
+          if (e.detail.result.element.closest(drupalSettings.editoria11y.hide_edit_links)) {
+            e.detail.tip.shadowRoot.querySelector('.ed11y-custom-edit-links')?.setAttribute('hidden', '');
+          }
+        });
+      }*/
+    }
+
+    const ed11y = new Ed11y(ed11yOptions); // eslint-disable-line
+    ed11yCustomTests();
+    ed11ySync();
+  }
+}
+
 // Call callback, init Editoria11y.
 ed11yReady(
   function () {
-    let ed11yOpts = document.getElementById('editoria11y-init');
-    if (!!ed11yOpts && window.location.href.indexOf('elementor-preview') === -1) {
-      ed11yOptions = JSON.parse(ed11yOpts.innerHTML);
-      ed11yOptions.customTests = 1;
-      ed11yOptions.panelNoCover = '#edac-highlight-panel'; // Accessibility Checker module
-
-      ed11yOptions.linkStringsNewWindows = ed11yOptions.linkStringsNewWindows ? new RegExp(ed11yOptions.linkStringsNewWindows, 'g') : /window|\stab|download/g;
-      if (ed11yOptions.title.length < 3) {
-        ed11yOptions.title = document.title;
-      }
-      ed11yOptions['checkVisible'] = ed11yOptions['checkVisible'] === 'true';
-      // When triggered by the in-editor "issues" link, force assertive.
-      if (window.location.href.indexOf('preview=true') > -1) {
-        ed11yOptions['alertMode'] = 'assertive';
-      }
-      if (window.location.href.indexOf('ed1ref') > -1) {
-        ed11yOptions['alertMode'] = 'assertive';
-        ed11yOptions['showDismissed'] = true;
-      }
-      ed11yOptions.cssUrls = [ed11yOptions.cssLocation];
-
-	  const ed11y = new Ed11y(ed11yOptions); // eslint-disable-line
-      ed11yCustomTests();
-      ed11ySync();
+      window.setTimeout(()=>{
+        ed11yInit();
+      },0);
     }
-  }
 );
